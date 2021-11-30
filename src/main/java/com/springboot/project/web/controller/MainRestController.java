7128cca5f7fa1ac70fae99f8eab76ce6a63922c7
조성규 <blueland1998@naver.com>
2021-11-30 17:47:50 +0900

    쓸데없는 파일 없애기

--- src/main/java/com/springboot/project/web/controller/MainRestController.java
+++
@@ -1,9 +0,0 @@
-/*package com.springboot.project.web.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MainRestController {
-
-}
-*/
--- src/main/java/com/springboot/project/web/model/beans/LikesBean.java
+++
@@ -1,6 +0,0 @@
-/*package com.springboot.project.web.model.beans;
-
-public class LikesBean {
-
-}
-*/
